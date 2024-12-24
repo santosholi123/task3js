21731c2636af096ddd681f398cc858299754dffd
santosholi123 <santosholi1128@gmail.com>
2024-12-24 19:35:50 +0545

    first commit

---
+++ T3.js
@@ -0,0 +1,35 @@
+// //Write a program using for loop to print Numbers from 1 to 10.
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// }
+//Write a program using while loop to sum of Numbers from 1 to 100.
+// let sum=0;
+// let indext=1;
+// while (indext<=100){
+//     sum+indext;
+//     indext++;
+// }
+// Write a program using for loop to print all even numbers between 1 and 20.
+// for(i=2; i<20; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+// Write a program using while loop to reverse the string (Softwarica)
+// let input = "Softwarica", reversed = "", i = input.length - 1;
+// while (i >= 0) reversed += input[i--];
+// console.log(reversed);
+// Write a program to find the largest number of Array [5, 8, 12, 3, 7]
+// const numbers = [5, 8, 12, 3, 7];
+// let largest = numbers[0];
+
+// for (let num of numbers) {
+//     if (num > largest) {
+//         largest = num;
+//     }
+// } console.log("The largest number is:", largest);
+// Write a program using a for loop to print the multiplication table of a given number (e.g., 5).
+for (let i = 1; i <= 10; i++) {
+    console.log(5 + " x " + i + " = " + 5 * i);
+}
+
